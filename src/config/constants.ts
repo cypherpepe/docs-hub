f8d1de6caf8798bba179ab2ec9633d5d199d73ae
Cypher Pepe <125112044+cypherpepe@users.noreply.github.com>
2024-10-11 19:33:39 +0300

    refactor: constants.ts
    - Consolidated external links into a reusable `LINKS` object for improved readability and maintainability.
    - Updated navigation structure to reference the `LINKS` object, reducing redundancy and simplifying updates to URLs.
    - Retained original functionality and ensured all links are still correctly referenced throughout the file.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,16 +1,26 @@
 import { join } from 'path';
 
+const BASE_URL = 'https://fuel.network';
+const TESTNET = 'testnet';
+const TESTNET_VERSION = 'v1';
+
 export const DOCS_DIRECTORY = join(process.cwd(), './docs');
 export const NIGHTLY_DOCS_DIRECTORY = join(process.cwd(), './docs/nightly');
 export const FIELDS = ['title', 'slug', 'content', 'category'];
 
-export const FUEL_TESTNET = 'testnet';
-export const FUEL_TESTNET_UPPER_CASE = 'Testnet';
-export const TESTNET_VERSION = 'v1';
-export const FAUCET_LINK = `https://faucet-${FUEL_TESTNET}.fuel.network/`;
-export const PLAYGROUND_LINK = `https://${FUEL_TESTNET}.fuel.network/${TESTNET_VERSION}/playground/`;
-export const EXPLORER_LINK = 'https://app.fuel.network/';
-export const BRIDGE_LINK = 'https://app.fuel.network/bridge';
+export const LINKS = {
+  faucet: `https://faucet-${TESTNET}.${BASE_URL}/`,
+  playground: `https://${TESTNET}.${BASE_URL}/${TESTNET_VERSION}/playground/`,
+  explorer: `https://app.${BASE_URL}/`,
+  bridge: `https://app.${BASE_URL}/bridge`,
+  swayCoreLib: 'https://fuellabs.github.io/sway/master/core/',
+  swayStdLib: 'https://fuellabs.github.io/sway/master/std/',
+  swayExamples: 'https://github.com/FuelLabs/sway-examples/',
+  swayApplications: 'https://github.com/FuelLabs/sway-applications/',
+  swayPlayground: 'https://sway-playground.org/',
+  audits: 'https://github.com/FuelLabs/audits',
+  contributing: '/docs/contributing',
+};
 
 export type LinkObject = {
   name: string;
@@ -19,135 +29,53 @@ export type LinkObject = {
 };
 
 export const EXTERNAL_NAVIGATION_LINKS: LinkObject[] = [
-  {
-    name: 'Sway Core Lib',
-    link: 'https://fuellabs.github.io/sway/master/core/',
-  },
-  {
-    name: 'Sway Std Lib',
-    link: 'https://fuellabs.github.io/sway/master/std/',
-  },
-  {
-    name: 'Sway Examples',
-    link: 'https://github.com/FuelLabs/sway-examples/',
-  },
-  {
-    name: 'Example Applications',
-    link: 'https://github.com/FuelLabs/sway-applications/',
-  },
-  {
-    name: 'Sway Playground',
-    link: 'https://sway-playground.org/',
-  },
-  {
-    name: 'Faucet',
-    link: FAUCET_LINK,
-  },
-  {
-    name: 'Audits',
-    link: 'https://github.com/FuelLabs/audits',
-  },
-  {
-    name: 'Contributing',
-    link: '/docs/contributing',
-  },
+  { name: 'Sway Core Lib', link: LINKS.swayCoreLib },
+  { name: 'Sway Std Lib', link: LINKS.swayStdLib },
+  { name: 'Sway Examples', link: LINKS.swayExamples },
+  { name: 'Example Applications', link: LINKS.swayApplications },
+  { name: 'Sway Playground', link: LINKS.swayPlayground },
+  { name: 'Faucet', link: LINKS.faucet },
+  { name: 'Audits', link: LINKS.audits },
+  { name: 'Contributing', link: LINKS.contributing },
 ];
 
 // TODO: replace this
 // this is only used the search component
 // biome-ignore lint/suspicious/noExplicitAny:
-export const NAVIGATION: any[] = [
-  {
-    name: 'Guides',
-    link: '/guides',
-  },
-  {
-    name: 'Intro',
-    link: '/docs/intro',
-  },
+export const NAVIGATION: LinkObject[] = [
+  { name: 'Guides', link: '/guides' },
+  { name: 'Intro', link: '/docs/intro' },
   {
     name: 'Sway',
     menu: [
-      {
-        name: 'Sway Language',
-      },
-      {
-        name: 'Sway',
-        link: '/docs/sway',
-      },
-      {
-        name: 'Standard Library',
-        link: 'https://fuellabs.github.io/sway/master/std/',
-      },
-      {
-        name: 'Core Library',
-        link: 'https://fuellabs.github.io/sway/master/core/',
-      },
-      {
-        name: 'Sway Playground',
-        link: 'https://sway-playground.org',
-      },
-      {
-        name: 'Sway By Example',
-        link: 'https://swaybyexample.com/',
-      },
-      {
-        name: 'Example Apps',
-        link: 'https://github.com/FuelLabs/sway-applications/',
-      },
-      {
-        name: 'Tooling',
-      },
-      {
-        name: 'Forc',
-        link: '/docs/forc',
-      },
+      { name: 'Sway Language' },
+      { name: 'Sway', link: '/docs/sway' },
+      { name: 'Standard Library', link: LINKS.swayStdLib },
+      { name: 'Core Library', link: LINKS.swayCoreLib },
+      { name: 'Sway Playground', link: LINKS.swayPlayground },
+      { name: 'Sway By Example', link: 'https://swaybyexample.com/' },
+      { name: 'Example Apps', link: LINKS.swayApplications },
+      { name: 'Tooling' },
+      { name: 'Forc', link: '/docs/forc' },
     ],
   },
   {
     name: 'SDKs',
     menu: [
-      {
-        name: 'Rust SDK',
-        link: '/docs/fuels-rs',
-      },
-      {
-        name: 'TypeScript SDK',
-        link: '/docs/fuels-ts',
-      },
-      {
-        name: 'Wallet',
-        link: '/docs/wallet/install/',
-      },
+      { name: 'Rust SDK', link: '/docs/fuels-rs' },
+      { name: 'TypeScript SDK', link: '/docs/fuels-ts' },
+      { name: 'Wallet', link: '/docs/wallet/install/' },
     ],
   },
   {
     name: 'Network',
     menu: [
-      // {
-      //   name: 'About Fuel',
-      //   link: '/docs/about-fuel/',
-      // },
-      {
-        name: 'GraphQL API',
-        link: '/docs/graphql/overview/',
-      },
-      {
-        name: 'Specifications',
-        link: '/docs/specs',
-      },
-      {
-        name: 'Explorer',
-        link: EXPLORER_LINK,
-      },
-      {
-        name: 'Faucet',
-        link: FAUCET_LINK,
-      },
-      {
-        name: 'Bridge',
-        link: BRIDGE_LINK,
-      },
+      //{ name: 'About Fuel', link: '/docs/about-fuel/' }
+      { name: 'GraphQL API', link: '/docs/graphql/overview/' },
+      { name: 'Specifications', link: '/docs/specs' },
+      { name: 'Explorer', link: LINKS.explorer },
+      { name: 'Faucet', link: LINKS.faucet },
+      { name: 'Bridge', link: LINKS.bridge },
     ],
   },
 ];
